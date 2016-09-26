d23f2563a11a97ebe844854454c98b4103a5d74e
Jack Collier <jackcollier@jacks-macbook-air.local>
2016-09-26 17:56:12 -0500

    Assignment 1 commit

---
+++ Assignments/assignment 1/assignment 1/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  assignment 1
+//
+//  Created by Jack Collier on 9/18/16.
+//  Copyright © 2016 Jack Collier. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
